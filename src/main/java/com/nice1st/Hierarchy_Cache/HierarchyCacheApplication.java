bb7b22496f61d822dfa2030219c59d6b8a614098
최영화 <sasimi86cyh@gmail.com>
2025-07-29 11:43:18 +0900

    feat

---
+++ src/main/java/com/nice1st/Hierarchy_Cache/HierarchyCacheApplication.java
@@ -0,0 +1,13 @@
+package com.nice1st.Hierarchy_Cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierarchyCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HierarchyCacheApplication.class, args);
+	}
+
+}
